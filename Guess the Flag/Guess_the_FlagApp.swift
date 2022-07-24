17f06972c8787cc8c9a64b108b30ae975896a0ef
mcantop <maciejcantop@icloud.com>
2022-07-24 20:53:25 +0200

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Maciej on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
